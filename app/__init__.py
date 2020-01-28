77cbaf88a2a188d007ed492953ac897197061519
root <root@redlure-server.9.6.235.vultr.com>
2020-01-28 15:32:00 -0500

    console switch

--- app/__init__.py
+++ app/__init__.py
@@ -20,21 +20,19 @@ login.login_view = 'login'
 
 from app import routes, models
 
-# TODO: change this to 'if not app.debug'
-if True:
 
-    # create logs dir if it does not exist
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-    
-    # log using rotatingfilehandler, capping log files at 10240 bytes and storing up to 10 logfiles
-    file_handler = RotatingFileHandler('logs/redure-server.log', maxBytes=10240, backupCount=10)
+# create logs dir if it does not exist
+if not os.path.exists('logs'):
+    os.mkdir('logs')
 
-    # Set the format and level for the log messages
-    formatter = logging.Formatter(fmt='%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]', datefmt='%m-%d-%y %H:%M')
-    file_handler.setFormatter(formatter)
-    file_handler.setLevel(logging.DEBUG)
+# log using rotatingfilehandler, capping log files at 10240 bytes and storing up to 10 logfiles
+file_handler = RotatingFileHandler('logs/redure-console.log', maxBytes=10240, backupCount=10)
 
-    # Add the handler and set the required level
-    app.logger.addHandler(file_handler)
-    app.logger.setLevel(logging.DEBUG)
+# Set the format and level for the log messages
+formatter = logging.Formatter(fmt='%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]', datefmt='%m-%d-%y %H:%M')
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
+
+# Add the handler and set the required level
+app.logger.addHandler(file_handler)
+app.logger.setLevel(logging.DEBUG)
---
+++ redlure-console.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python3
+from app import app, db, functions
+from app.models import User, Profile, Role, Workspace, List, Person, Email, Page, Domain, Campaign, Result, Server, APIKey, Form, Campaignpages, WorkerCampaignSchema, CipherTest
+from app.functions import Color
+import subprocess
+import os
+import shlex
+import shutil
+from config import Config
+from datetime import datetime
+from cryptography.fernet import InvalidToken
+from app.cipher import Cipher, new_cipher_key, encrypt, decrypt
+
+
+# objects to initialize 'flask shell' with
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        'db': db,
+        'User': User,
+        'Profile': Profile,
+        'Role': Role,
+        'Workspace': Workspace,
+        'List': List,
+        'Person': Person,
+        'Email': Email,
+        'Page': Page,
+        'Domain': Domain,
+        'Campaign': Campaign,
+        'Result': Result,
+        'Server': Server,
+        'APIKey': APIKey,
+        'Form': Form,
+        'Campaignpages': Campaignpages
+    }
+
+
+def init_cipher():
+    passphrase = ''
+    print(f'{Color.gray}[*] redlure encrypts sensitive database fields{Color.end}')
+    print(f'{Color.gray}[*] Enter a passphrase that will be used in generating the key\n{Color.end}')
+    while passphrase == '':
+        passphrase =  input(f'{Color.gray}[+] Passphrase: {Color.red}').encode()
+    print(f'\n[!] WARNING: Do not lose your passphrase - doing so will result in losing access to parts of your database{Color.end}')
+    
+    new_cipher_key(passphrase)
+    
+    input(f'\n{Color.gray}[+] Press enter to continue: {Color.end}')
+
+
+def get_cipher():
+    passphrase = input(f'{Color.gray}[+] Enter the cipher passphrase: {Color.red}').encode()
+    new_cipher_key(passphrase)
+    cipher_text = CipherTest.query.first().value
+    try:
+        plain_text = decrypt(cipher_text)
+        print(f'[+] {plain_text.decode()}\n{Color.end}')
+    except InvalidToken:
+        print(f'\n[!] Decryption failed - invalid passphrase{Color.end}')
+        exit()            
+
+def init_db():
+    if os.path.isdir('migrations'):
+        shutil.rmtree('migrations')
+
+    print(f'\n{Color.red}[*] Creating database\n{Color.end}')
+
+    proc = subprocess.Popen(shlex.split('flask db init'))
+    proc.wait()
+    proc = subprocess.Popen(shlex.split('flask db migrate'))
+    proc.wait()
+    proc = subprocess.Popen(shlex.split('flask db upgrade'))
+    proc.wait()
+
+    print(f'\n{Color.red}[+] Initializing database values\n{Color.end}')
+
+    general_ws = Workspace(name='General')
+    db.session.add(general_ws)
+    db.session.commit()
+
+    administrator = Role(name='redlure admin', role_type='Administrator')
+    general_ws = Workspace.query.filter_by(id=1, name='General').first()
+    if general_ws is not None:
+        administrator.workspaces.append(general_ws)
+
+    db.session.add(administrator)
+    db.session.commit()
+
+    admin = User(username='admin', role_id=1)
+    admin.set_password('redlure')
+    db.session.add(admin)
+    db.session.commit()
+
+    encrypted_val = encrypt(b'Bingo. Welcome to redlure')
+    cipher_test = CipherTest(value=encrypted_val)
+    db.session.add(cipher_test)
+    db.session.commit()
+
+    key = APIKey()
+
+# check for scheduled campaigns that need to be rentered into the queue
+def check_campaigns():
+    campaigns = Campaign.query.filter_by(status='Scheduled').all()
+    for campaign in campaigns:
+        if datetime.now() < campaign.start_time:
+            schema = WorkerCampaignSchema()
+            campaign_data = schema.dump(campaign)
+            campaign.cast(campaign_data)
+
+        else:
+            campaign.status = 'Start time missed (server outage)'
+            db.session.commit()
+
+def gen_certs():
+    proc = subprocess.Popen(shlex.split('openssl req -x509 -newkey rsa:4096 -nodes -subj "/" -out redlure-cert.pem -keyout redlure-key.pem -days 365'))
+    proc.wait()
+
+
+def banner():
+    print(f'''
+{Color.red}                   .___{Color.gray}.__                        {Color.end}     
+{Color.red}_______   ____   __| _/{Color.gray}|  |  __ _________   ____  {Color.end} 
+{Color.red}\_  __ \_/ __ \ / __ | {Color.gray}|  | |  |  \_  __ \_/ __ \ {Color.end}
+{Color.red} |  | \/\  ___// /_/ | {Color.gray}|  |_|  |  /|  | \/\  ___/ {Color.end}
+{Color.red} |__|    \___  >____ | {Color.gray}|____/____/ |__|    \___  >{Color.end}
+{Color.red}             \/     \/ {Color.gray}                        \/  {Color.end}
+    ''')
+
+
+if __name__ == '__main__':
+    banner()
+    # SECRET_KEY is required
+    if Config.SECRET_KEY == '':
+        print('[!] A secret key is required - set the SECRET_KEY attribute in config.py')
+        print(f'[!] New suggested random secret key: {os.urandom(24)}')
+        exit()
+
+    # check if db exists yet
+    if not os.path.isfile('redlure.db'):
+        init_cipher()
+        init_db()
+    else:
+        get_cipher()
+        check_campaigns()
+
+    # generate certs if they dont exist
+    if Config.CERT_PATH == 'redlure-cert.pem' and Config.KEY_PATH == 'redlure-key.pem':
+        if not os.path.isfile('redlure-cert.pem') or not os.path.isfile('redlure-key.pem'):
+            gen_certs()
+
+    # start the server
+    app.logger.info('redlure-console starting up')
+    #server = subprocess.Popen(['gunicorn', 'app:app', '-b 0.0.0.0:5000', '--certfile', Config.CERT_PATH, '--keyfile', Config.KEY_PATH])
+    #server.wait()
+    app.run(host='0.0.0.0', ssl_context=(Config.CERT_PATH, Config.KEY_PATH), use_reloader=False)
