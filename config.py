17d3ea4e96dfc2c9a4c49dc36ea04b73a25a4c3d
puzzleaches <nanastasi@sprocketsecurity.com>
2022-03-18 13:08:59 +0000

    yeet

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = ''
+    SECRET_KEY = 'test'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'redlure.db' + '?check_same_thread=False')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CERT_PATH = 'redlure-cert.pem'
     KEY_PATH = 'redlure-key.pem'
-    PASSPHRASE = ''
+    PASSPHRASE = 'test'
