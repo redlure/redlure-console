d9b9b43a4de0e37f489adcd0e0a748a18edced3c
Matt Creel <37981031+Tw1sm@users.noreply.github.com>
2022-03-26 12:38:27 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = 'test'
+    SECRET_KEY = ''
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'redlure.db' + '?check_same_thread=False')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CERT_PATH = 'redlure-cert.pem'
